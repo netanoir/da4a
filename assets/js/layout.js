f220a40bf5e836b21fbe905cefae805e4face964
neta <netanyabomani@gmail.com>
2020-08-09 19:36:29 -0400

    update styles

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,6 +1,7 @@
 const backgrounds = [
     "blm-the-all-nite-images.png",
     "la-riots-1992-by-ricky-bonilla.png",
+    "new-york-protest-against-vietnam-war.png",
     "pan-african-conference-protests.png",
     "tulsa-race-riot-inflames-1921.png",
     "woolworths-sit-in-durham-nc-1960.png",
@@ -13,7 +14,6 @@ const backgrounds = [
   
   window.onload = (event) => {
     let updateBackground = document.getElementById("section-highlight").style.backgroundImage = openTag + fileDirectory + selectBackgroundFile + closeTag;
-    // console.log(updateBackground);
   };
   
   
