5f9f75843b0bef659a51db0797253c5fa9f88ba3
neta <netanyabomani@gmail.com>
2020-08-23 20:10:59 -0400

    mobile alert

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -27,7 +27,7 @@ for (let i = 0, linksLength = links.length; i < linksLength; i += 1) {
 window.onload=function(){
   var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
   if (mobile) {
-      alert("Please read this web zine on a Dekstop, laptop computer, or tablet in landscape orientation for the best reading experience.");              
+      alert("Please read this web zine in landscape orientation on a desktop, laptop or tablet for the best reading experience.");              
   } else {
 
   }
